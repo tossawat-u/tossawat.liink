9253e35d6a182a095ec867f390b68ffc2b91d415
Tossawat Umpharote <t_umpharote@hotmail.com>
2023-04-18 11:28:11 +0700

    First commit code.

---
+++ assets/config.js
@@ -0,0 +1,47 @@
+let config = {
+  "Title": "Tossawat Umpharote - Links",
+  "Name": "Tossawat Umpharote (M)",
+  "Description": "Hello, My contact point is the below description.",
+  "links": [
+    {
+      "Title": "E-Contact Card",
+      "URL": "https://tossawat.in.th/contact_profiles/tossawat@cititex.co.th.vcf",
+      "icon_classes": "fas fa-address-card"
+    },
+    {
+      "Title": "Email",
+      "URL": "mailto:tu@tossawat.in.th",
+      "icon_classes": "fas fa-envelope"
+    },
+    {
+      "Title": "LinkedIn",
+      "URL": "https://www.linkedin.com/",
+      "icon_classes": "fab fa-linkedin"
+    },
+    {
+      "Title": "Facebook",
+      "URL": "https://www.facebook.com/mmmm.MiiMii/",
+      "icon_classes": "fab fa-facebook"
+    },
+    {
+      "Title": "Twitter",
+      "URL": "https://twitter.com/m_fantasy",
+      "icon_classes": "fab fa-twitter"
+    },
+    {
+      "Title": "Youtube",
+      "URL": "https://www.youtube.com/channel/UCdQNi6vISplTALMLPsHTXzg",
+      "icon_classes": "fab fa-youtube"
+    },
+    {
+      "Title": "Website",
+      "URL": "https://tossawat.in.th/",
+      "icon_classes": "fas fa-link",
+    },
+    {
+      "Title": "Github",
+      "URL": "https://github.com/tossawat-u/",
+      "icon_classes": "fab fa-github",
+    },
+  ]
+}
